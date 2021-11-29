f84554486e866c166e167bbf480f027758161026
PleaseLetMe(yinhong) <yinhong@xunlei.com>
2021-11-29 13:15:02 +0800

    默认导出

--- 如何维护Symbol.js
+++ 如何维护Symbol.js
@@ -1,8 +1,14 @@
 /**
  * 问题：Symbol 既然作为“指纹”，但如何保存（或者说更好维护）这些指纹最好呢？？
+ * 思考过程：
+ * 指纹？每个人都有独特的指纹
+ * 假设指纹对应Symbol、人对应模块、存款对应数据
+ * 所以模块才是主导，推动Symbol去匹配对应数据的发起者
+ *
  * 个人见解：
  * 保存Symbol绝对不是目的。依据其唯一性，管理与其绑定的数据，才是真的目的
- * 所以我没有用Symbol做value而是用Symbol做key
+ * 所以我没有用Symbol做value而是用Symbol做key，也许随机数也能实现，但随机数依旧有重复的可能，
+ * 但在一个库的底层封装中，是不允许有如此误差出现的，Symbol才是作为唯一标识的最佳选择。
  *
  * 第三方库数据管理工具（简易版）（取名模仿vue与vueX）
  *  注：在js里面无法写类型判断，在ts里面Symbol不能作为key使用
@@ -10,27 +16,30 @@
 class SymbolX {
   constructor() {}
   data = {};
+  moduleMap = new Map();
   /**
-   * @param {symbol} symbolVal 库标识
-   * @param {any} identifyLabel 库数据（理论上无类型限制，推荐json）
-   * @returns {array} 会返回当前库中，一共有哪些库标识，方便后续根据库标识获取库数据
+   * @param {symbol} symbolVal 模块标识
+   * @param {any} identifyLabel 模块数据（理论上无类型限制，推荐json）
+   * @returns {array} 会返回当前库中，一共有哪些模块标识，方便后续根据模块标识获取库数据
    */
-  setState(symbolVal, identifyLabel) {
-    let data = this.data;
+  setState(module, symbolVal, identifyLabel) {
+    let { data, moduleMap } = this;
     data[symbolVal] = identifyLabel;
-    return Reflect.ownKeys(data);
+    moduleMap.set(module, symbolVal);
+    return moduleMap;
   }
   /**
-   * @param {symbol} symbolVal 库标识
+   * @param {symbol} symbolVal 模块标识
    * @returns 返回当前库标识对应的库数据
    */
-  getState(symbolVal) {
-    return this.data[symbolVal];
+  getState(module) {
+    let temp = this.moduleMap.get(module);
+    return this.data[temp];
   }
 }
-export let symbolObj = new SymbolX();
+let symbolObj = new SymbolX();
 
 // 测试区域=====================================================================
 let test = Symbol();
-console.log(symbolObj.setState(test, [1, 2, 3])); // [Symbol()]
-console.log(symbolObj.getState(test)); // [1,2,3]
+console.log(symbolObj.setState("导航栏模块", test, [1, 2, 3])); // [["导航栏模块",Symbol()]]
+console.log(symbolObj.getState("导航栏模块")); // [1,2,3]
